3bfd937d3d7ffc85aba7879cfb15b317ce9877c6
Rostyslav Paliuha <r.paliuha@1plus1.tv>
2023-03-30 17:45:40 +0300

    added windows restart script

---
+++ autoRun.bat
@@ -0,0 +1,7 @@
+ECHO Kill previous trading bot process
+taskkill /FI "WindowTitle eq trading_bot" /T /F
+ECHO Start trading bot process
+start "trading_bot" java -jar E:\development\projects\trading-bot\target\trading-bot-0.2.jar
+
+
+
